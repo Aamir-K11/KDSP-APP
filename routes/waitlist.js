2075de3350a9ad63f765ec6f26f76cda1bb8f20d
Aamir-K11 <amirkhan11999@gmail.com>
2021-09-18 22:35:40 +0500

    Bulk Update Waitlist

--- routes/waitlist.js
+++ routes/waitlist.js
@@ -10,18 +10,15 @@ waitlistRoute.use(express.json());
 waitlistRoute.get("/",(req,res)=>{
         FamilyModel.find({state:"Approved"})
                    .then(value=>{
-                        value.forEach(element => {
-                            Waitlist = new WaitlistModel({
-                                family: element
-                            });
-    
-                            Waitlist.save().then(subvalue=>{return res.status(202).json({status: "Waitlist Updated"})})
-                                            .catch(err => {return res.status(500).json({error: err})});
-                        });
+                            console.log(value);
+                            WaitlistModel.create(value).then(result => {
+                                return res.status(202).json({status: "Success"})})
+                                                       .catch(err=>{return res.status(500).json({status: err})});
+                        }).catch(err=>{return res.status(500).json({error: err})});;
                        
-                   })
-                   .catch(err=>{return res.status(500).json({error: err})});
-});
+                   });
+                  
 
 
-module.exports = waitlistRoute;
+
+module.exports = waitlistRoute; 
